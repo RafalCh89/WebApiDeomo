53b20f57f86be1397c68188fd9eccad2374ff1e6
RafalCh89 <rafal.choma@hotmail.com>
2017-04-23 15:09:13 +0100

    Add project files.

---
+++ WebApiDeomo/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApiDeomo.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ WebApiDeomo/Controllers/EmployeesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Web.Http;
+using WebAPIDemo.Models;
+
+namespace WebAPIDemo.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        Employee[] employees = new Employee[]{
+         new Employee { ID = 1, Name = "Rafal", JoiningDate =
+            DateTime.Parse(DateTime.Today.ToString()), Age = 28 },
+         new Employee { ID = 2, Name = "Lukasz", JoiningDate =
+            DateTime.Parse(DateTime.Today.ToString()), Age = 26 },
+         new Employee { ID = 3, Name = "TOm", JoiningDate =
+            DateTime.Parse(DateTime.Today.ToString()), Age = 31 }
+      };
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return employees;
+        }
+
+        public IHttpActionResult GetEmployee(int id)
+        {
+            var employee = employees.FirstOrDefault((p) => p.ID == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+    }
+}
